c33b0566d902d6dfe2d22455843e447abbebf9ef
akhilesh <akhilespande@gmail.com>
2019-01-22 15:56:03 +0530

    springboot , restful, jdbc template & oracle example

---
+++ src/main/java/com/akhilesh/service/IArticleService.java
@@ -0,0 +1,13 @@
+package com.akhilesh.service;
+
+import com.akhilesh.entity.Article;
+
+import java.util.List;
+
+public interface IArticleService {
+     List<Article> getAllArticles();
+     Article getArticleById(int articleId);
+     boolean addArticle(Article article);
+     void updateArticle(Article article);
+     void deleteArticle(int articleId);
+}
